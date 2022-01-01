6542d5c26c02416241260dcbf1decf5876b39c34
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-01 20:11:22 +0100

    add bareturn images from props

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,5 +1,21 @@
-const Images = () => {
-  return <div></div>;
+const Images = (props) => {
+  return (
+    <div>
+      {props.images.map((image) => {
+        return (
+          <div className="">
+            <img
+              src={image.url}
+              alt={image.name}
+              key={image.name}
+              onClick={() => props.handleClick(image.name)}
+            />
+            <h2>{image.name}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Images;
