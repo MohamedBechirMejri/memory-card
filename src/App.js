5aceb09e87eb8a01c1a2ffbc6862a702869b1f4d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-01 19:19:57 +0100

    add rules

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ const App = () => {
   return (
     <div>
       <h1>Detective Conan Memory Game</h1>
-     
+      <p>
+        Get points by clicking on an image but don't click on any more than
+        once! You move to next level when you click on all the images.
+      </p>
     </div>
   );
 };
