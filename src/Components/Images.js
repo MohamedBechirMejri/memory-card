7db4e3113316eb8d8df28f70a8bf56a5ab627b0c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-02 01:19:20 +0100

    more tweaks

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,17 +1,19 @@
 const Images = (props) => {
   return (
-    <div className="flex ">
+    <div className="flex flex-wrap items-center justify-center gap-4 transition-all ">
       {props.images.map((image) => {
         return (
-          <div className="flex flex-col items-center flex-wrap">
+          <div
+            className="flex flex-col items-center max-w-[27%]    "
+            key={image.name}
+          >
             <img
               src={image.url}
               alt={image.name}
-              key={image.name}
               onClick={() => props.handleClick(image.name)}
-              className="w-2/3 rounded-xl shadow"
+              className="   rounded-xl shadow"
             />
-            <h2 className="text-[1.5em]">{image.name}</h2>
+            <h2 className="text-[1.5em]">{image.name} </h2>
           </div>
         );
       })}
