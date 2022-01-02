d0486446f8a63f147511949e073d82c3e865d218
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-02 02:28:53 +0100

    remove useless file

--- src/Components/Utils.js
+++
@@ -1,8 +0,0 @@
-export const shuffle = (array) => {
-  // return [...array].sort(() => Math.random() - 0.5);
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
