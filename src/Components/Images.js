68115f232db1f7412e54d04afa027846ead63c78
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-02 06:08:36 +0100

    more stylign

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,7 +1,7 @@
 const Images = (props) => {
   return (
     <div
-      className="flex flex-wrap items-center justify-center gap-8 transition-all bg-[#aaaa] p-8 m-4 rounded-3xl "
+      className="flex flex-wrap items-center justify-center gap-8 transition-all bg-[#aaaa] p-4 m-0 rounded-3xl "
       style={{
         backdropFilter: "blur(5px)",
       }}
@@ -9,16 +9,16 @@ const Images = (props) => {
       {props.images.map((image) => {
         return (
           <div
-            className="flex flex-col items-center max-w-[27%] transition-all hover:scale-105  "
+            className="flex flex-col items-center  transition-all hover:scale-105  "
             key={image.name}
           >
             <img
               src={image.url}
               alt={image.name}
               onClick={() => props.handleClick(image.name)}
-              className="rounded-xl transition-all active:scale-95 ring-4 ring-white shadow-2xl h-44"
+              className="rounded-xl transition-all active:scale-95 ring-4 ring-white shadow-2xl max-w-[27vw]"
             />
-            <h2 className="text-[1.5em] transition-all">{image.name} </h2>
+            <h2 className="text-[1.5em]   transition-all">{image.name} </h2>
           </div>
         );
       })}
