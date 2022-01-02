0f903ef0c15ac5f206c3d49f45817fe6f66e5dce
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-02 01:19:42 +0100

    shuffle images on click

--- src/App.js
+++ src/App.js
@@ -2,31 +2,74 @@ import Stats from "./Components/Stats";
 import Images from "./Components/Images";
 import { useState } from "react";
 
+const shuffle = (array) => {
+  // return [...array].sort(() => Math.random() - 0.5);
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
 const App = () => {
   const [images, setImages] = useState([
+    // {
+    //   name: "Conan",
+    //   url: "https://www.detectiveconanworld.com/wiki/images/archive/9/9e/20110914002215%21Conan_Edogawa_60px.jpg",
+    // },
     {
-      name: "Conan",
-      url: "https://www.detectiveconanworld.com/wiki/images/archive/9/9e/20110914002215%21Conan_Edogawa_60px.jpg",
+      name: "Conan2",
+      url: "https://www.detectiveconanworld.com/wiki/images/0/00/Conan_Edogawa_Profile.png",
     },
     {
-      name: "Conan",
-      url: "https://www.detectiveconanworld.com/wiki/images/archive/9/9e/20110914002215%21Conan_Edogawa_60px.jpg",
+      name: "Kogoro",
+      url: "https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile2.png",
     },
     {
-      name: "Conan",
-      url: "https://www.detectiveconanworld.com/wiki/images/archive/9/9e/20110914002215%21Conan_Edogawa_60px.jpg",
+      name: "Ran",
+      url: "https://www.detectiveconanworld.com/wiki/images/9/92/Ran_Mouri_Profile.jpg",
+    },
+    {
+      name: "Shinshi",
+      url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
+    },
+    {
+      name: "Conan2",
+      url: "https://www.detectiveconanworld.com/wiki/images/0/00/Conan_Edogawa_Profile.png",
+    },
+    {
+      name: "Kogoro",
+      url: "https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile2.png",
+    },
+    {
+      name: "Ran",
+      url: "https://www.detectiveconanworld.com/wiki/images/9/92/Ran_Mouri_Profile.jpg",
+    },
+    {
+      name: "Shinshi",
+      url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
+    },
+    {
+      name: "Shinshi",
+      url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
     },
   ]);
+  const handleClick = (name) => {
+    setImages([]);
+    setTimeout(() => {
+      setImages(shuffle(images));
+    }, 1);
+  };
 
   return (
-    <div className="bg-gradient-to-t from-orange-600 to-pink-900 h-screen w-screen flex flex-col items-center justify-start text-center text-white   font-semibold gap-8 pt-8">
+    <div className="bg-gradient-to-t from-orange-600 to-pink-900 h-screen w-screen flex flex-col items-center justify-start text-center text-white   font-semibold gap-4 pt-8">
       <h1 className="text-[2em]  ">Detective Conan Memory Game</h1>
       <p className="  text-[1em]">
         Get points by clicking on an image but don't click on any more than
         once! You move to next level when you click on all the images.
       </p>
       <Stats />
-      <Images images={images} />
+      <Images images={images} handleClick={handleClick} />
     </div>
   );
 };
