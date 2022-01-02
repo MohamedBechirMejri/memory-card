51154a4ca2e0d76a0ac593669da30efc25644b33
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-02 03:56:00 +0100

    tweak styles

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,19 +1,19 @@
 const Images = (props) => {
   return (
-    <div className="flex flex-wrap items-center justify-center gap-4 transition-all ">
+    <div className="flex flex-wrap items-center justify-center gap-8 transition-all ">
       {props.images.map((image) => {
         return (
           <div
-            className="flex flex-col items-center max-w-[27%]    "
+            className="flex flex-col items-center max-w-[27%] transition-all hover:scale-105  "
             key={image.name}
           >
             <img
               src={image.url}
               alt={image.name}
               onClick={() => props.handleClick(image.name)}
-              className="   rounded-xl shadow"
+              className="   rounded-xl   transition-all active:scale-95 ring-4 ring-white shadow-2xl"
             />
-            <h2 className="text-[1.5em]">{image.name} </h2>
+            <h2 className="text-[1.5em] transition-all">{image.name} </h2>
           </div>
         );
       })}
