f23c61b44d2bbc4d7d12d0ac750d3c8405719666
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-02 05:55:35 +0100

    tweak img size

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -1,6 +1,11 @@
 const Images = (props) => {
   return (
-    <div className="flex flex-wrap items-center justify-center gap-8 transition-all ">
+    <div
+      className="flex flex-wrap items-center justify-center gap-8 transition-all bg-[#aaaa] p-8 m-4 rounded-3xl "
+      style={{
+        backdropFilter: "blur(5px)",
+      }}
+    >
       {props.images.map((image) => {
         return (
           <div
@@ -11,7 +16,7 @@ const Images = (props) => {
               src={image.url}
               alt={image.name}
               onClick={() => props.handleClick(image.name)}
-              className="   rounded-xl   transition-all active:scale-95 ring-4 ring-white shadow-2xl"
+              className="rounded-xl transition-all active:scale-95 ring-4 ring-white shadow-2xl h-44"
             />
             <h2 className="text-[1.5em] transition-all">{image.name} </h2>
           </div>
