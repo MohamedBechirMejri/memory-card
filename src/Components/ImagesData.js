537107b83263bbb5a0f3268551fd9fdfd3d7f3a4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-02 05:22:26 +0100

    add more characters

--- src/Components/ImagesData.js
+++ src/Components/ImagesData.js
@@ -16,23 +16,22 @@ const ImagesData = [
       name: "Shinichi Kudo",
       url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
     },
-  ],
-  [
+
     {
       name: "Ai Haibara",
-      url: "https://www.detectiveconanworld.com/wiki/File:Ai_Haibara_Profile.png",
+      url: "https://www.detectiveconanworld.com/wiki/images/4/4f/Ai_Haibara_Profile.png",
     },
     {
       name: "Ayumi Yoshida",
-      url: "https://www.detectiveconanworld.com/wiki/File:Ayumi_Yoshida_Profile.jpg",
+      url: "https://www.detectiveconanworld.com/wiki/images/7/74/Ayumi_Yoshida_Profile.jpg",
     },
     {
       name: "Mitsuhiko Tsuburaya",
-      url: "https://www.detectiveconanworld.com/wiki/File:Mitsuhiko_Tsuburaya_Profile.jpg",
+      url: "https://www.detectiveconanworld.com/wiki/images/7/7e/Mitsuhiko_Tsuburaya_Profile.jpg",
     },
     {
       name: "Genta Kojima",
-      url: "https://www.detectiveconanworld.com/wiki/File:Genta_Kojima_Profile2.jpg",
+      url: "https://www.detectiveconanworld.com/wiki/images/a/a9/Genta_Kojima_Profile2.jpg",
     },
     {
       name: "Hiroshi Agasa",
@@ -40,51 +39,171 @@ const ImagesData = [
     },
     {
       name: "Sonoko Suzuki",
-      url: "https://www.detectiveconanworld.com/wiki/File:Sonoko_Suzuki_Profile.jpg",
+      url: "https://www.detectiveconanworld.com/wiki/images/9/95/Sonoko_Suzuki_Profile.jpg",
     },
     {
       name: "Eri Kisaki",
-      url: "https://www.detectiveconanworld.com/wiki/File:Eri_Kisaki_Profile.png",
+      url: "https://www.detectiveconanworld.com/wiki/images/f/f3/Eri_Kisaki_Profile.png",
     },
     {
       name: "Makoto Kyogoku",
-      url: "https://www.detectiveconanworld.com/wiki/File:Makoto_Kyogoku_Profile.png",
-    },
-  ],
-  [
-    {
-      name: "Conan Edogawa",
-      url: "https://www.detectiveconanworld.com/wiki/images/0/00/Conan_Edogawa_Profile.png",
+      url: "https://www.detectiveconanworld.com/wiki/images/9/92/Makoto_Kyogoku_Profile.png",
     },
     {
-      name: "Kogoro Mouri",
-      url: "https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile2.png",
+      name: "Yoko Okino",
+      url: "https://www.detectiveconanworld.com/wiki/images/b/bd/Yoko_Okino_Profile.jpg",
     },
     {
-      name: "Ran Mouri",
-      url: "https://www.detectiveconanworld.com/wiki/images/9/92/Ran_Mouri_Profile.jpg",
+      name: "Kazuha Toyama",
+      url: "https://www.detectiveconanworld.com/wiki/images/7/77/Kazuha_Toyama_Profile.jpg",
     },
     {
-      name: "Shinichi Kudo",
-      url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
-    },
-  ],
-  [
-    {
-      name: "Conan Edogawa",
-      url: "https://www.detectiveconanworld.com/wiki/images/0/00/Conan_Edogawa_Profile.png",
+      name: "Heiji Hattori",
+      url: "https://www.detectiveconanworld.com/wiki/images/2/2c/Heiji_Hattori_Profile.png",
     },
     {
-      name: "Kogoro Mouri",
-      url: "https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile2.png",
+      name: "Yukiko Kudo",
+      url: "https://www.detectiveconanworld.com/wiki/images/f/f9/Yukiko_Kudo_Profile.png",
     },
     {
-      name: "Ran Mouri",
-      url: "https://www.detectiveconanworld.com/wiki/images/9/92/Ran_Mouri_Profile.jpg",
+      name: "Yusaku Kudo",
+      url: "https://www.detectiveconanworld.com/wiki/images/d/d4/Yusaku_Kudo_Profile.png",
     },
     {
-      name: "Shinichi Kudo",
-      url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
+      name: "Subaru Okiya",
+      url: "https://www.detectiveconanworld.com/wiki/images/c/ce/Subaru_Okiya_Profile.png",
+    },
+    {
+      name: "Masumi Sera",
+      url: "https://www.detectiveconanworld.com/wiki/images/8/84/Masumi_Sera_Profile.jpg",
+    },
+    {
+      name: "Curaçao",
+      url: "https://www.detectiveconanworld.com/wiki/images/8/86/Cura%C3%A7ao_Profile.jpg",
+    },
+    {
+      name: "Juzo Megure",
+      url: "https://www.detectiveconanworld.com/wiki/images/6/66/Juzo_Megure_Profile2.png",
+    },
+    {
+      name: "Miwako Sato",
+      url: "https://www.detectiveconanworld.com/wiki/images/4/45/Miwako_Sato_Profile.png",
+    },
+    {
+      name: "Wataru Takagi",
+      url: "https://www.detectiveconanworld.com/wiki/images/b/be/Wataru_Takagi_Profile2.png",
+    },
+    {
+      name: "Yumi Miyamoto",
+      url: "https://www.detectiveconanworld.com/wiki/images/2/2c/Yumi_Miyamoto_Profile.png",
+    },
+    {
+      name: "Naeko Miike",
+      url: "https://www.detectiveconanworld.com/wiki/images/7/72/Naeko.png",
+    },
+    {
+      name: "Yui Uehara",
+      url: "https://www.detectiveconanworld.com/wiki/images/4/47/Yui_Uehara_Profile.jpg",
+    },
+    {
+      name: "Rei Furuya/Bourboun",
+      url: "https://www.detectiveconanworld.com/wiki/images/8/86/Rei_Furuya_Profile.jpg",
+    },
+    {
+      name: "Shuichi Akai",
+      url: "https://www.detectiveconanworld.com/wiki/images/8/83/Shuichi_Akai_Profile.jpg",
+    },
+    {
+      name: "Jodie Starling",
+      url: "https://www.detectiveconanworld.com/wiki/images/5/5d/Jodie_Starling_Profile.png",
+    },
+    {
+      name: "Hidemi Hondou/Kir",
+      url: "https://www.detectiveconanworld.com/wiki/images/5/55/Hidemi_Hondou_Profile.jpg",
+    },
+    {
+      name: "Mary Sera",
+      url: "https://www.detectiveconanworld.com/wiki/images/f/f4/Mary_Sera_Profile.jpg",
+    },
+    {
+      name: "The Boss",
+      url: "https://www.detectiveconanworld.com/wiki/images/1/16/Renya_Karasuma_Profile.jpg",
+    },
+    {
+      name: "Gin",
+      url: "https://www.detectiveconanworld.com/wiki/images/8/89/S6-Gin.png",
+    },
+    {
+      name: "Vodka",
+      url: "https://www.detectiveconanworld.com/wiki/images/2/22/Vodka_Profile.jpg",
+    },
+    {
+      name: "Vermouth",
+      url: "https://www.detectiveconanworld.com/wiki/images/1/1b/EP813-Vermouth.jpg",
+    },
+    {
+      name: "Chianti",
+      url: "https://www.detectiveconanworld.com/wiki/images/a/af/Chianti_Profile.jpg",
+    },
+    {
+      name: "Korn",
+      url: "https://www.detectiveconanworld.com/wiki/File:Korn_Profile.jpg",
+    },
+    {
+      name: "Shiho Miyano",
+      url: "https://www.detectiveconanworld.com/wiki/images/b/bc/Shiho_Miyano_Profile.jpg",
+    },
+    {
+      name: "Akemi Miyano",
+      url: "https://www.detectiveconanworld.com/wiki/images/a/af/Akemi_Miyano_Profile.jpg",
+    },
+    {
+      name: "Elena Miyano",
+      url: "https://www.detectiveconanworld.com/wiki/images/7/77/Elena_Miyano_Profile.png",
+    },
+    {
+      name: "Kaitou Kid",
+      url: "https://www.detectiveconanworld.com/wiki/images/d/d1/Kaitou_Kid_Profile.png",
+    },
+    {
+      name: "Aoko Nakamori",
+      url: "https://www.detectiveconanworld.com/wiki/File:Aoko_Nakomori_Profile.jpg",
+    },
+    {
+      name: "Kaito Kuroba",
+      url: "https://www.detectiveconanworld.com/wiki/images/9/96/Kaito_Kuroba_Profile.jpg",
+    },
+    {
+      name: "Ginzo Nakamori",
+      url: "https://www.detectiveconanworld.com/wiki/images/2/23/Ginzo_Nakamori_Profile.png",
+    },
+    {
+      name: "Akako Koizumi",
+      url: "https://www.detectiveconanworld.com/wiki/images/e/e2/Akako_Koizumi_Profile.jpg",
+    },
+    {
+      name: "Saguru Hakuba",
+      url: "https://www.detectiveconanworld.com/wiki/images/d/df/Saguru_Hakuba_Profile.jpg",
+    },
+    {
+      name: "Konosuke Jii",
+      url: "https://www.detectiveconanworld.com/wiki/images/5/52/Konosuke_Jii_Profile.jpg",
+    },
+    {
+      name: "Chikage Kuroba",
+      url: "https://www.detectiveconanworld.com/wiki/images/3/36/Chikage_Kuroba_Profile.jpg",
+    },
+    {
+      name: "Toichi Kuroba",
+      url: "https://www.detectiveconanworld.com/wiki/images/c/c9/Toichi_Kuroba_Profile.jpg",
+    },
+    {
+      name: "The Culprit",
+      url: "https://www.detectiveconanworld.com/wiki/images/a/a0/BossBlackshadow.jpg",
+    },
+    {
+      name: "Scar Akai",
+      url: "https://www.detectiveconanworld.com/wiki/images/9/90/Scar_Akai_Profile.png",
     },
   ],
 ];
