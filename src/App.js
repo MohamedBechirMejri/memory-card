acfd455d29caa6d473d7a8f223516ffb1742702f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-02 01:25:42 +0100

    move images to own file

--- src/App.js
+++ src/App.js
@@ -2,50 +2,10 @@ import Stats from "./Components/Stats";
 import Images from "./Components/Images";
 import { useState } from "react";
 import { shuffle } from "./Components/Utils";
+import { ImagesData } from './Components/ImagesData';
 
 const App = () => {
-  const [images, setImages] = useState([
-    // {
-    //   name: "Conan",
-    //   url: "https://www.detectiveconanworld.com/wiki/images/archive/9/9e/20110914002215%21Conan_Edogawa_60px.jpg",
-    // },
-    {
-      name: "Conan2",
-      url: "https://www.detectiveconanworld.com/wiki/images/0/00/Conan_Edogawa_Profile.png",
-    },
-    {
-      name: "Kogoro",
-      url: "https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile2.png",
-    },
-    {
-      name: "Ran",
-      url: "https://www.detectiveconanworld.com/wiki/images/9/92/Ran_Mouri_Profile.jpg",
-    },
-    {
-      name: "Shinshi",
-      url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
-    },
-    {
-      name: "Conan2",
-      url: "https://www.detectiveconanworld.com/wiki/images/0/00/Conan_Edogawa_Profile.png",
-    },
-    {
-      name: "Kogoro",
-      url: "https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile2.png",
-    },
-    {
-      name: "Ran",
-      url: "https://www.detectiveconanworld.com/wiki/images/9/92/Ran_Mouri_Profile.jpg",
-    },
-    {
-      name: "Shinshi",
-      url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
-    },
-    {
-      name: "Shinshi",
-      url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
-    },
-  ]);
+  const [images, setImages] = useState(ImagesData[0]);
   const handleClick = (name) => {
     setImages([]);
     setTimeout(() => {
---
+++ src/Components/ImagesData.js
@@ -0,0 +1,44 @@
+export const ImagesData = [
+  [
+    // {
+    //   name: "Conan",
+    //   url: "https://www.detectiveconanworld.com/wiki/images/archive/9/9e/20110914002215%21Conan_Edogawa_60px.jpg",
+    // },
+    {
+      name: "Conan2",
+      url: "https://www.detectiveconanworld.com/wiki/images/0/00/Conan_Edogawa_Profile.png",
+    },
+    {
+      name: "Kogoro",
+      url: "https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile2.png",
+    },
+    {
+      name: "Ran",
+      url: "https://www.detectiveconanworld.com/wiki/images/9/92/Ran_Mouri_Profile.jpg",
+    },
+    {
+      name: "Shinshi",
+      url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
+    },
+    {
+      name: "Conan2",
+      url: "https://www.detectiveconanworld.com/wiki/images/0/00/Conan_Edogawa_Profile.png",
+    },
+    {
+      name: "Kogoro",
+      url: "https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile2.png",
+    },
+    {
+      name: "Ran",
+      url: "https://www.detectiveconanworld.com/wiki/images/9/92/Ran_Mouri_Profile.jpg",
+    },
+    {
+      name: "Shinshi",
+      url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
+    },
+    {
+      name: "Shinshi",
+      url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
+    },
+  ],
+];
