4439f0a3af99083148ef815acf5eef8af385077d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-02 04:03:39 +0100

    add more characters

--- src/Components/ImagesData.js
+++ src/Components/ImagesData.js
@@ -1,43 +1,89 @@
 const ImagesData = [
   [
-    // {
-    //   name: "Conan",
-    //   url: "https://www.detectiveconanworld.com/wiki/images/archive/9/9e/20110914002215%21Conan_Edogawa_60px.jpg",
-    // },
     {
-      name: "Conan2",
+      name: "Conan Edogawa",
       url: "https://www.detectiveconanworld.com/wiki/images/0/00/Conan_Edogawa_Profile.png",
     },
     {
-      name: "Kogoro",
+      name: "Kogoro Mouri",
       url: "https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile2.png",
     },
     {
-      name: "Ran",
+      name: "Ran Mouri",
       url: "https://www.detectiveconanworld.com/wiki/images/9/92/Ran_Mouri_Profile.jpg",
     },
     {
-      name: "Shinshi",
+      name: "Shinichi Kudo",
       url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
     },
+  ],
+  [
     {
-      name: "Conan2",
+      name: "Ai Haibara",
+      url: "https://www.detectiveconanworld.com/wiki/File:Ai_Haibara_Profile.png",
+    },
+    {
+      name: "Ayumi Yoshida",
+      url: "https://www.detectiveconanworld.com/wiki/File:Ayumi_Yoshida_Profile.jpg",
+    },
+    {
+      name: "Mitsuhiko Tsuburaya",
+      url: "https://www.detectiveconanworld.com/wiki/File:Mitsuhiko_Tsuburaya_Profile.jpg",
+    },
+    {
+      name: "Genta Kojima",
+      url: "https://www.detectiveconanworld.com/wiki/File:Genta_Kojima_Profile2.jpg",
+    },
+    {
+      name: "Hiroshi Agasa",
+      url: "https://www.detectiveconanworld.com/wiki/images/thumb/d/d5/Hiroshi_Agasa_Profile.jpg/275px-Hiroshi_Agasa_Profile.jpg",
+    },
+    {
+      name: "Sonoko Suzuki",
+      url: "https://www.detectiveconanworld.com/wiki/File:Sonoko_Suzuki_Profile.jpg",
+    },
+    {
+      name: "Eri Kisaki",
+      url: "https://www.detectiveconanworld.com/wiki/File:Eri_Kisaki_Profile.png",
+    },
+    {
+      name: "Makoto Kyogoku",
+      url: "https://www.detectiveconanworld.com/wiki/File:Makoto_Kyogoku_Profile.png",
+    },
+  ],
+  [
+    {
+      name: "Conan Edogawa",
       url: "https://www.detectiveconanworld.com/wiki/images/0/00/Conan_Edogawa_Profile.png",
     },
     {
-      name: "Kogoro",
+      name: "Kogoro Mouri",
       url: "https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile2.png",
     },
     {
-      name: "Ran",
+      name: "Ran Mouri",
       url: "https://www.detectiveconanworld.com/wiki/images/9/92/Ran_Mouri_Profile.jpg",
     },
     {
-      name: "Shinshi",
+      name: "Shinichi Kudo",
       url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
     },
+  ],
+  [
     {
-      name: "Shinshi",
+      name: "Conan Edogawa",
+      url: "https://www.detectiveconanworld.com/wiki/images/0/00/Conan_Edogawa_Profile.png",
+    },
+    {
+      name: "Kogoro Mouri",
+      url: "https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile2.png",
+    },
+    {
+      name: "Ran Mouri",
+      url: "https://www.detectiveconanworld.com/wiki/images/9/92/Ran_Mouri_Profile.jpg",
+    },
+    {
+      name: "Shinichi Kudo",
       url: "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
     },
   ],
