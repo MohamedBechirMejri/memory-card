d6a3723691d416e5fe598432ade01923a31c8bc6
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-02 02:29:21 +0100

    add data to stats component

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -1,7 +1,11 @@
-const Stats = () => {
-    return <div>
-      <h1>hi</h1>
-  </div>;
+const Stats = (props) => {
+  return (
+    <div className="text-right text-[1.1em]">
+      <h2>Level {props.level}</h2>
+      <h2>Score: {props.score}</h2>
+      <h2>Top Score: {props.topScore}</h2>
+    </div>
+  );
 };
 
 export default Stats;
