7b67797e79413c3cbde336015d43993c2e70142c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-01-02 01:23:42 +0100

    move shuffle function to own file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import Stats from "./Components/Stats";
 import Images from "./Components/Images";
 import { useState } from "react";
-
-const shuffle = (array) => {
-  // return [...array].sort(() => Math.random() - 0.5);
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
+import { shuffle } from "./Components/Utils";
 
 const App = () => {
   const [images, setImages] = useState([
